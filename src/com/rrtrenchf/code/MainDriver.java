56d408bfb7722b77e7b959ce613e6c1800fd2314
rrtrenchf <rrtrenchf@gmail.com>
2022-08-01 15:07:20 -0400

    input works

--- src/com/rrtrenchf/code/MainDriver.java
+++ src/com/rrtrenchf/code/MainDriver.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 
 
-public class MainDriver extends thermometer {
+public class MainDriver {
 
 	public MainDriver() {
 		// TODO Auto-generated constructor stub
@@ -11,10 +11,30 @@ public class MainDriver extends thermometer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner in = new Scanner(System.in);
-System.out.print("Name your thermometer");
+Scanner scan = new Scanner(System.in);
+System.out.println("Name your thermometer");
 
-	String name = new name(System.in);
+	String name = scan.nextLine();
+	System.out.println("Your thermometer is called "+ name);
+	
+	System.out.println("Enter degrees in farenheight");
+	int degreesF = scan.nextInt();
+	System.out.println("You entered "+ degreesF + "F");
+	
+	System.out.println("Enter degrees in Celcium");
+	int degreesC = scan.nextInt();
+	System.out.println("You entered "+ degreesC + "C");
+	
+	System.out.println("enter 3 temp readings for the average temp");
+	
+	int  tempReadings[] = new int[3];
+	  for (int i = 0 ; i < tempReadings.length; i++ ) {
+          tempReadings[i] = scan.nextInt();
+          
+          System.out.println("You entered " + tempReadings[i]);
+       }
+	
+	
 	}
 
 }
--- src/com/rrtrenchf/code/thermometer.java
+++ src/com/rrtrenchf/code/thermometer.java
@@ -1,10 +1,77 @@
 package com.rrtrenchf.code;
 
+
 public class thermometer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		private int degreesF;
+		private int degreesC;
+		private String name;
+		
+		private int getDegreesF() {
+			return degreesF;
+		}
 
-	}
+
+
+		private void setDegreesF(int degreesF) {
+			this.degreesF = degreesF;
+		}
+
+
+
+		private int getDegreesC() {
+			return degreesC;
+		}
+
+
+
+		private void setDegreesC(int degreesC) {
+			this.degreesC = degreesC;
+		}
+
+
+
+		private String getName() {
+			return name;
+		}
+
+
+
+		private void setName(String name) {
+			this.name = name;
+		}
+
+
+
+		private int[] getTempReadings() {
+			return tempReadings;
+		}
+
+
+
+		private void setTempReadings(int[] tempReadings) {
+			this.tempReadings = tempReadings;
+		}
+
+
+
+		private int [] tempReadings = new int [3];
+		
+		public static int convertToFahrenheit(int c) {
+			return;  
+			
+		}
+		public static int convertToCelsius(int f) {
+			return;
+		}
+		public static void calculateAvgTemp(int[] temps) {
+			
+		}
+
+	
+
+
 
 }
+
