8b86a1fad8958ef789690cff1678e8f87173961a
rrtrenchf <rrtrenchf@gmail.com>
2022-08-01 14:09:48 -0400

    first commit

---
+++ src/com/rrtrenchf/code/MainDriver.java
@@ -0,0 +1,20 @@
+package com.rrtrenchf.code;
+import java.util.Scanner;
+
+
+
+public class MainDriver extends thermometer {
+
+	public MainDriver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in = new Scanner(System.in);
+System.out.print("Name your thermometer");
+
+	String name = new name(System.in);
+	}
+
+}
---
+++ src/com/rrtrenchf/code/thermometer.java
@@ -0,0 +1,10 @@
+package com.rrtrenchf.code;
+
+public class thermometer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
